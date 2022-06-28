42d493c326e59370d471a448aafec916f2952834
Gerardo Guevara <gguevara@cikume.com>
2022-06-28 17:56:28 -0600

    update

--- src/Heroes/components/HeroList.jsx
+++ src/Heroes/components/HeroList.jsx
@@ -3,12 +3,18 @@ import { useMemo } from "react";
 import { getHeroesByPublisher } from "../helpers";
 
 export const HeroList = ({ publisher }) => {
+
+  // se utiliza use memo porque solo es data de lectura y jamas va a cambiar
+  // se escucha si el elemento publisher cambia
   const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
 
   return (
     <div className="row rows-cols-1 row-cols-md-3 g-3">
       {heroes.map((hero) => (
-        <div>{JSON.stringify(hero)}</div>
+       <>
+       <spam>{JSON.stringify(hero)}</spam>
+       <br />
+       </>
       ))}
     </div>
   );
