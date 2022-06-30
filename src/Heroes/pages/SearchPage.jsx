86c4897b7c71d7a1b03c12a30bfdfe2c8732f543
Gerguevara <33437557+Gerguevara@users.noreply.github.com>
2022-06-29 22:58:25 -0600

    search

--- src/Heroes/pages/SearchPage.jsx
+++ src/Heroes/pages/SearchPage.jsx
@@ -8,25 +8,33 @@ import { getHeroesByName } from "../helpers";
 const SearchPage = () => {
   // toma el navigate de react router dom
   const navigate = useNavigate();
-  //toma la url actual de la ruta con este Hook
+  //toma la url actual de la ruta en la que nos encontramos con este Hook
   const location = useLocation();
-
+// parsea el string que es urel en secciones por ejemplo aca toma q
+// pero podrian venir mas elementos, entonces aca de search?q= toma su value
+// si el valor no viene el valor de q es '' por defecto
   const { q = "" } = queryString.parse(location.search);
   const heroes = getHeroesByName(q);
 
+  // el usuario no ha escrito nada 
   const showSearch = q.length === 0;
+  // el usuario busco algo y no lo encontro
   const showError = q.length > 0 && heroes.length === 0;
 
   //use form para manejar el estado de el formulario se le paso el valor
   //q que toma de  los query params, para que asi al nomas entre la url el valor del input sea seteado
+  // recordar toma el valor del name  del input con el cual se hace referencia
   const { searchText, onInputChange } = useForm({
     searchText: q,
   });
 
   const onSearchSubmit = (event) => {
     event.preventDefault();
+    // valida que tenga 2 o mas caracteres
     // if ( searchText.trim().length <= 1 ) return;
 
+    //simula a una navegacion a estaa ruta, si no se le indica el sufijo
+    // es como que navegue a esta misma mas lo que el texto de los templates literales
     navigate(`?q=${searchText}`);
   };
 
